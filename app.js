5cc93aafbb4df3d6b2ade2d7c489b3957356445f
HUGO DE OLIVEIRA ALVES VIEIRA <25203638@CFP127ALUNO.LOCAL>
2026-05-11 08:26:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+async function getDadosCep(cep){
+    const url = `https://brasilapi.com.br/api/cep/v2/${cep}`
+    const response = fetch(url)
+    const data = (await response).json()
+    return data
+}
+
+async function preencherFormulario(){
+    const cep = document.getElementById('cep').value 
+    const dadosCep = await getDadosCep(cep)
+    document.getElementById('endereco').value = dadosCep.street
+    document.getElementById('bairro').value = dadosCep.neighborhood
+    document.getElementById('cidade').value = dadosCep.city
+    document.getElementById('estado').value = dadosCep.state
+}
+
+ document.getElementById('cep').addEventListener('focusout',preencherFormulario)
